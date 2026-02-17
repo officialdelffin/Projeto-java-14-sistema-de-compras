8b7facf3ba640885a3282d8e5d0397775d485cfe
officialdelffin <workdelffin@gmail.com>
2026-02-17 15:57:08 -0300

    commit 3

--- src/br/com/officialdelffin/MainExecution/Main.java
+++ src/br/com/officialdelffin/MainExecution/Main.java
@@ -11,16 +11,37 @@ public class Main {
     private int userChoice;
 
 
-    // Metodos :
+    // Metodos Getters e Setters :
 
+    // Getters :
+
+    public int getAccountBalance() {
+
+
+        return accountBalance;
+
+
+    }
+
+
+    // Setters :
+
+    public void setAccountBalance(int accountBalance) {
+
+
+        this.accountBalance = accountBalance;
+
+
+    }
+
+
+    // Metodos :
 
     // Metodo de execução do sistema :
     public static void main(String[] args) {
 
 
-
         System.out.printf("Seja bem-vindo ao sistema de compras! \n");
-
 
 
     }
