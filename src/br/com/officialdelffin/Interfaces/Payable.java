0c080b4263557f2560db627607703bda804f49d8
officialdelffin <workdelffin@gmail.com>
2026-02-17 18:50:54 -0300

    commit 4

--- src/br/com/officialdelffin/Interfaces/Payable.java
+++ src/br/com/officialdelffin/Interfaces/Payable.java
@@ -1,9 +1,13 @@
+
+// Pacotes :
 package br.com.officialdelffin.Interfaces;
 
+
+// Interface que implemtenta e obriga um sistema de pagamento :
 public interface Payable {
 
 
-
+    int payment ();
 
 
 }
--- src/br/com/officialdelffin/MainExecution/Main.java
+++ src/br/com/officialdelffin/MainExecution/Main.java
@@ -1,6 +1,11 @@
+
+// Pacotes :
 package br.com.officialdelffin.MainExecution;
 
+
+// Importações :
 import java.util.Scanner;
+
 
 // Classe principal :
 public class Main {
@@ -41,7 +46,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.printf("Seja bem-vindo ao sistema de compras! \n");
+        System.out.printf("Seja bem-vindo ao sistema de compras \n");
 
 
     }
